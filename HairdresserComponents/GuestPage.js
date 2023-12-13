dea1e528eee693652bb29330a4f1b930fcfea904
Pernille Hopfner <90635224+pernillehopfner@users.noreply.github.com>
2023-12-13 17:27:52 +0100

    Update GuestPage.js

--- HairdresserComponents/GuestPage.js
+++ HairdresserComponents/GuestPage.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { View , Text, StyleSheet } from 'react-native';
-import SignUpForm from "./Auth/SignUpForm"
-import LoginForm from "./Auth/LoginForm"
+// import React from 'react';
+// import { View , Text, StyleSheet } from 'react-native';
+// import SignUpForm from "./Auth/SignUpForm"
+// import LoginForm from "./Auth/LoginForm"
 
-function GuestPage({ navigation }) {
-  return (
-    <View style={styles.container}>      
-    <Text>Barber login</Text>
-    <SignUpForm />
-    <LoginForm />
-</View>
-  );
-}
+// function GuestPage({ navigation }) {
+//   return (
+//     <View style={styles.container}>      
+//     <Text>Barber login</Text>
+//     <SignUpForm />
+//     <LoginForm />
+// </View>
+//   );
+// }
 
-export default GuestPage;
+// export default GuestPage;
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
